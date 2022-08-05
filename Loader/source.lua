21d1c6b9927961bc440bac02b67983f6edf70e19
GhostDuckyy <95024755+GhostDuckyy@users.noreply.github.com>
2022-08-05 15:02:53 +0800

    Update source.lua

--- Loader/source.lua
+++ Loader/source.lua
@@ -1,3 +1,10 @@
-local game = {
-
+local game_list = {
+    ["1257535190"] = "https://raw.githubusercontent.com/GhostDuckyy/Bread/main/Storage/Godzilla_Sim.lua",
 }
+
+for i,v in next, (game_list) do
+    print(i,v)
+    if game.PlaceId == tonumber(i) then
+        loadstring(game:HttpGet(v,true))()
+    end
+end
