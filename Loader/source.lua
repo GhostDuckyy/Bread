0020db917efe227ea7f9f9ac6177005a07acba36
GhostDuckyy <95024755+GhostDuckyy@users.noreply.github.com>
2022-08-06 15:29:59 +0800

    loader

--- Loader/source.lua
+++ Loader/source.lua
@@ -2,13 +2,14 @@ local game_list = {
     -- Godzilla Simulator
     ["1257535190"] = "https://raw.githubusercontent.com/GhostDuckyy/Bread/main/Storage/Godzilla_Sim.lua",
     -- Rebirth Champions X
-    ["8540346411"] = "",
+    ["8540346411"] = "https://raw.githubusercontent.com/GhostDuckyy/Bread/main/Storage/Rebirth%20Champions%20X.lua",
 }
 
 for i,v in next, (game_list) do
     if game.PlaceId == tonumber(i) then
-        if v ~= nil then
-            loadstring(game:HttpGet(v,true))()
+        if v ~= nil and type(v) == "string" then
+            local url = v;
+            loadstring(game:HttpGet(url,true))()
         end
     end
 end
