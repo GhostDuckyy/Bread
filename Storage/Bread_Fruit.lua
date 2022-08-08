55b77b9c08c480a5bbcc38ce31c41964e77b41b3
GhostDuckyy <95024755+GhostDuckyy@users.noreply.github.com>
2022-08-08 20:29:15 +0800

    bread

--- Storage/Bread_Fruit.lua
+++ Storage/Bread_Fruit.lua
@@ -1,3 +1,11 @@
 local str = "aHR0cHM6Ly9naXN0LmdpdGh1Yi5jb20vR2hvc3REdWNreXkvZmJjNjM4NmY1ODI3YjllZGZjMDUxM2Y1ZTUwNWQ1ZWUvcmF3"
-local url = syn and syn.crypt.base64.decode(str) or base64_decode(str)
-loadstring(game:HttpGet(url,true))()
+function decode(strs)
+    local code = tostring(strs)
+    if  syn and syn.crypt.base64.decode then
+        return syn.crypt.base64.decode(code)
+    else
+        return base64_decode(code)
+    end
+end
+local url = decode(str)
+loadstring(game:HttpGet(tostring(url),true))()
