b3daf439ac513de9e581755c952f6eb000a554d2
GhostDuckyy <95024755+GhostDuckyy@users.noreply.github.com>
2022-08-08 20:08:11 +0800

    bread fruit

--- Storage/Bread_Fruit.lua
+++ Storage/Bread_Fruit.lua
@@ -1,3 +1,3 @@
 local str = "aHR0cHM6Ly9naXN0LmdpdGh1Yi5jb20vR2hvc3REdWNreXkvZmJjNjM4NmY1ODI3YjllZGZjMDUxM2Y1ZTUwNWQ1ZWUvcmF3"
-local url = base64_decode(str)
+local url = syn and syn.crypt.base64.decode(str) or base64_decode(str)
 loadstring(game:HttpGet(url,true))()
