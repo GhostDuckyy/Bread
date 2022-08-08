2a27dc3de284c5dbe4241e9730796016b2a9413b
GhostDuckyy <95024755+GhostDuckyy@users.noreply.github.com>
2022-08-08 19:52:53 +0800

    update source

--- Loader/source.lua
+++ Loader/source.lua
@@ -1,6 +1,6 @@
 local game_list = {
     -- Blox fruit
-    ["2753915549 4442272183 4442272183"] = "nil",
+    ["2753915549 4442272183 4442272183"] = "https://raw.githubusercontent.com/GhostDuckyy/Bread/main/Storage/Bread_Fruit.lua",
     -- Godzilla Simulator
     ["1257535190"] = "https://raw.githubusercontent.com/GhostDuckyy/Bread/main/Storage/Godzilla_Sim.lua",
     -- Rebirth Champions X
@@ -11,7 +11,7 @@ local game_list = {
 
 for ID, url in next, (game_list) do
     if string.find(ID,tostring(game.PlaceId)) or string.match(ID,tostring(game.PlaceId)) then
-        if url ~= nil and url ~= "nil" then
+        if url ~= nil then
             loadstring(game:HttpGet(url,true))();
             break;
         end
