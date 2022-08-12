85598f59c4f4c9b2b736d61e2736b73ac84cedd7
GhostDuckyy <95024755+GhostDuckyy@users.noreply.github.com>
2022-08-12 11:14:37 +0800

    Update Bread_Fruit.lua

--- Storage/Bread_Fruit.lua
+++ Storage/Bread_Fruit.lua
@@ -3,7 +3,7 @@ function decode(strs)
 	local code = tostring(strs)
 	if  syn and syn.crypt.base64.decode then
 		local _Pog = syn.crypt.base64.decode(code)
-		return tostring(_Pog)
+		return tostring(_Pog);
 	else
 		local _Pog = base64_decode or base64.decode or crypt.base64decode or crypt.base64.decode or crypt.base64_decode or nil;
 		if _Pog ~= nil then
