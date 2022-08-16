33c59c87f91e163cc3e93523eece34b24d05395b
GhostDuckyy <95024755+GhostDuckyy@users.noreply.github.com>
2022-08-16 21:00:20 +0800

    Updated RebithChampionsX.lua

--- Storage/Rebirth Champions X.lua
+++ Storage/Rebirth Champions X.lua
@@ -19,6 +19,8 @@ local RunService = game:GetService("RunService")
 local HttpService = game:GetService("HttpService")
 
 local Click_remote = game:GetService("ReplicatedStorage").Events:FindFirstChild("Click3")
+local Hatch_function = game:GetService("ReplicatedStorage").Functions:FindFirstChild("Unbox")
+
 --// Setting
 getgenv().Setting = {
     Auto_click = false,
@@ -26,13 +28,61 @@ getgenv().Setting = {
         Selected = nil,
         Islands = {},
     },
+    Eggs = {
+        Selected = "Basic",
+        Triple = false,
+        Hatch = false,
+    }
 }
 
 
 --// Script
 local main = w:AddFolder("Main")
+local pet = w:AddFolder("Pets")
 local tp = w:AddFolder("Teleport")
 local misc = w:AddFolder("Misc")
+
+local pet_list = {}
+for i,v in next, (game:GetService("ReplicatedStorage").Assets.Eggs:GetChildren()) do
+    if v:IsA("Model") and not string.find(tostring(v.Name),"Robux") then
+        if not table.find(pet_list,tostring(v.Name)) then
+            table.insert(pet_list,tostring(v.Name))
+        end
+    end
+end
+pet:AddList({text = "Select Eggs",flag = nil,value = "Basic",values = pet_list,callback = function(x)
+    getgenv().Setting.Eggs.Selected = x;
+end})
+pet:AddToggle({text = "Triple",state = false,callback = function(x)
+    getgenv().Setting.Eggs.Triple = x;
+end})
+pet:AddToggle({text = "Auto hatch",state = false,callback = function(x)
+    getgenv().Setting.Eggs.Hatch = x;
+    if x then
+        Auto_hatch()
+    end
+end})
+function Auto_hatch()
+    spawn(function()
+        while getgenv().Setting.Eggs.Hatch and task.wait(.1) do
+            if Hatch_function then
+                if getgenv().Setting.Eggs.Triple then
+                    local arg = {
+                        [1] = tostring(getgenv().Setting.Eggs.Selected),
+                        [2] = "Triple",
+                    }
+                    Hatch_function:InvokeServer(unpack(arg))
+                else
+                    local arg = {
+                        [1] = tostring(getgenv().Setting.Eggs.Selected),
+                        [2] = "Single",
+                    }
+                    Hatch_function:InvokeServer(unpack(arg))
+                end
+            end
+        end
+    end)
+end
 
 main:AddToggle({text = "Auto click",state = false,callback = function(x)
     getgenv().Setting.Auto_click = x;
@@ -55,9 +105,10 @@ end
 main:AddButton({text = "Best world boost",callback = function()
     local World_Boost = game:GetService("ReplicatedStorage").Events:FindFirstChild("WorldBoost")
     local Stored_World_Boost = game:GetService("ReplicatedStorage").Events:FindFirstChild("StoreWorldBoost")
+    local Latest_world = "Shadow"
     if World_Boost and Stored_World_Boost then
-        World_Boost:FireServer("Neon")
-        Stored_World_Boost:FireServer("Neon")
+        World_Boost:FireServer(tostring(Latest_world))
+        Stored_World_Boost:FireServer(tostring(Latest_world))
     end
 end})
 
@@ -73,7 +124,7 @@ main:AddButton({text = "Unlock all gamepases", function()
     end
 end})
 
-main:AddButton({text = "Unlock all portal",callback = function()
+main:AddButton({text = "Unlock all portal&world",callback = function()
     local path = game:GetService("Workspace").Scripts.Portals
     for i,v in ipairs(path:GetDescendants()) do
         if v:IsA("Model") then
@@ -132,6 +183,14 @@ if workspace:WaitForChild("Scripts") and workspace["Scripts"]:WaitForChild("Tele
     })
 end
 
+misc:AddButton({
+    text = "Discord Invite",
+    callback = function()
+        if setclipboard then
+            setclipboard("https://discord.gg/TFUeFEESVv")
+        end
+    end
+})
 misc:AddBind({
     text = "Toggle UI",
     key = Enum.KeyCode.RightControl,
