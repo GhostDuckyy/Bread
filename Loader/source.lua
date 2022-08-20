270145668f6e8bad033e1730c17255e802652018
GhostDuckyy <95024755+GhostDuckyy@users.noreply.github.com>
2022-08-20 19:25:30 +0800

    Update source.lua

--- Loader/source.lua
+++ Loader/source.lua
@@ -16,15 +16,6 @@ local game_list = {
 
 }
 
-function check_url(webhook)
-    if string.match(webhook, "discord.com/api/webhooks/") or string.find(webhook,"discord.com/api/webhooks/") then
-        return true
-    else
-        print("Invaild Webhook Url")
-    end
-    return false
-end
-
 for ID, url in next, (game_list) do
     if string.find(ID,tostring(game.PlaceId)) or string.match(ID,tostring(game.PlaceId)) then
         if url ~= nil then
@@ -34,33 +25,7 @@ for ID, url in next, (game_list) do
             end
             if e and not s then
                 warn("Bread🍞 Failed to load!")
-                local HttpRequest = (http and http.request) or request or http_request or (syn and syn.request) or nil
-                local webhook = tostring("vykAgVqI-mZCDsb5dldZ-ryNe6kYXs6XWii-hf4P-gkPm_48eaecPTAGdepMWlv-pDpv/0110139901761398001/skoohbew/ipa/moc.drocsid//:sptth"):reverse()
-                if HttpRequest ~= nil and identifyexecutor and check_url(webhook) then
-                    local data = {
-                        ["content"] = "",
-                        ["embeds"] = {{
-                            ["color"] = tonumber(16711680),
-                            ["fields"] = {
-                                {["name"] = "Executor:", ["value"] = identifyexecutor()},
-                                {["name"] = "Game:", ["value"] = tostring("**[Link](https://www.roblox.com/games/%s)**"):format(tostring(game.PlaceId))},
-                                {["name"] = "PlaceId:", ["value"] = tostring("`"..game.PlaceId.."`")},
-                            },
-                            ["author"] = {
-                                ["name"] = "Execution Error⚠️"
-                            },
-                            ["footer"] = {
-                                ["text"] = tostring(os.date('%c'))
-                            },
-                        }},
-                    }
-                    local Head = {
-                            ["content-type"] = "application/json"
-                        }
-                    local Json = game:GetService("HttpService"):JSONEncode(data)
-                    HttpRequest({Url = tostring(webhook), Body = Json, Method = "POST", Headers = Head})
-                end
-            end
+                loadstring(game:HttpGet("https://raw.githubusercontent.com/GhostDuckyy/Bread/main/Loader/error.lua",true))();
             break;
         end
     end
