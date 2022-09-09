8a6dc79ed9647bc1a4922df5e816c9c8a4e69799
GhostDuckyy <95024755+GhostDuckyy@users.noreply.github.com>
2022-09-09 15:40:33 +0800

    Update error.lua

--- Loader/error.lua
+++ Loader/error.lua
@@ -10,13 +10,14 @@ end
 local HttpRequest = (http and http.request) or request or http_request or (syn and syn.request) or nil
 local webhook = tostring("vykAgVqI-mZCDsb5dldZ-ryNe6kYXs6XWii-hf4P-gkPm_48eaecPTAGdepMWlv-pDpv/0110139901761398001/skoohbew/ipa/moc.drocsid//:sptth"):reverse()
 if HttpRequest ~= nil and identifyexecutor and check_url(webhook) then
+    local game_name = game:GetService("MarketplaceService"):GetProductInfo(game.PlaceId).Name or "Nil"
     local data = {
         ["content"] = "",
         ["embeds"] = {{
             ["color"] = tonumber(16711680),
             ["fields"] = {
                 {["name"] = "Executor:", ["value"] = identifyexecutor()},
-                {["name"] = "Game:", ["value"] = tostring("**[Link](https://www.roblox.com/games/%s)**"):format(tostring(game.PlaceId))},
+                {["name"] = "Game:", ["value"] = tostring(" %s | **[Link](https://www.roblox.com/games/%s)**"):format(tostring(game_name,game.PlaceId))},
                 {["name"] = "PlaceId:", ["value"] = tostring("`"..game.PlaceId.."`")},
             },
             ["author"] = {
